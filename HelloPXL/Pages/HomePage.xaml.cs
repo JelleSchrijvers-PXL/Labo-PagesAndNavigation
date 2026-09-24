e53865c1823a58641d4288bfd2355665fe9e7c6a
JelleSchrijvers-PXL <schrijversjelle07@gmail.com>
2026-09-24 13:54:39 +0200

    Routing functions toegepast

--- HelloPXL/Pages/HomePage.xaml.cs
+++ HelloPXL/Pages/HomePage.xaml.cs
@@ -7,13 +7,13 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
 	}
 
-    private void RoutingMenu_Clicked(object sender, EventArgs e)
+    private async void RoutingMenu_Clicked(object sender, EventArgs e)
     {
-
+        await Shell.Current.GoToAsync(nameof(MenuPage));
     }
 
-    private void RoutingAbout_Clicked(object sender, EventArgs e)
+    private async void RoutingAbout_Clicked(object sender, EventArgs e)
     {
-
+        await Shell.Current.GoToAsync(nameof(AboutPage));
     }
 }
