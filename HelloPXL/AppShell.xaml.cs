e4b1257873ca280fc1869146b1d8be307950b21d
JelleSchrijvers-PXL <schrijversjelle07@gmail.com>
2026-09-24 13:37:49 +0200

    Appshell routing toegevoegd

--- HelloPXL/AppShell.xaml.cs
+++ HelloPXL/AppShell.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace HelloPXL
+﻿using HelloPXL.Pages;
+
+namespace HelloPXL
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-
+            Routing.RegisterRoute(nameof(HelloPage), typeof(HelloPage));
         }
     }
 }
